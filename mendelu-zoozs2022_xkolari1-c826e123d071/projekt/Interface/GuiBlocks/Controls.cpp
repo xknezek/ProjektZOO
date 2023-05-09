7cdf24a768db7ad210b62afd06b3b0749317c79e
xknezek <129303129+xknezek@users.noreply.github.com>
2023-05-09 11:10:01 +0200

    Update Controls.cpp
    Comments

--- mendelu-zoozs2022_xkolari1-c826e123d071/projekt/Interface/GuiBlocks/Controls.cpp
+++ mendelu-zoozs2022_xkolari1-c826e123d071/projekt/Interface/GuiBlocks/Controls.cpp
@@ -5,11 +5,13 @@
 #include "Controls.h"
 #include "..\InterfaceRenderer.h"
 
+//Konstruktor pro třídu Controls.
 Controls::Controls(int x, int y, int width, int height, int id) : RenderableBlock(x, y, width, height, id) {
 
 }
 
+//Metoda pro vypsání ovládání.
 void Controls::render(GameState *gameState) {
-    print(0, 0, "[pohyb:sipky] [exit:e] [weapons:w] [potions:p]      ZOO 2022/23 xkolari1, xpecink1, xsalaj, xzilavy");
+    print(0, 0, "[pohyb:sipky] [exit:e] [weapons:w] [potions:p]                                                     ");
     gameState->setNeedsRender(InterfaceRenderer::UI_CONTROLS, false);
 }

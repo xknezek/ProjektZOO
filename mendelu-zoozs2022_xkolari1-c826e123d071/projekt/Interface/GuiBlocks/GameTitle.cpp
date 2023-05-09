430e4e390bef8ac593a62d1a0fac5683adada259
xknezek <129303129+xknezek@users.noreply.github.com>
2023-05-09 11:15:12 +0200

    Update GameTitle.cpp
    Comments

--- mendelu-zoozs2022_xkolari1-c826e123d071/projekt/Interface/GuiBlocks/GameTitle.cpp
+++ mendelu-zoozs2022_xkolari1-c826e123d071/projekt/Interface/GuiBlocks/GameTitle.cpp
@@ -5,6 +5,7 @@
 #include "GameTitle.h"
 #include "../InterfaceRenderer.h"
 
+//Metoda pro výpis názvu hry do středu obrazovky a rámečku kolem.
 void GameTitle::render(GameState *gameState) {
     //Render hlavicky
     //fillWithChar(char(196)); //─
@@ -13,6 +14,7 @@ void GameTitle::render(GameState *gameState) {
     gameState->setNeedsRender(InterfaceRenderer::UI_HEADER, false);
 }
 
+//Konstruktor pro třídu GameTitle.
 GameTitle::GameTitle(int x, int y, int width, int height, int id) : RenderableBlock(x, y, width, height, id) {
 
 }

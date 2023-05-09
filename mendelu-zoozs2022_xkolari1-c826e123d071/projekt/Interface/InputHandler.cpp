3d60968dfb14684f483b7faa69b870c5fd511bc0
xknezek <129303129+xknezek@users.noreply.github.com>
2023-05-09 11:38:44 +0200

    Update InputHandler.cpp

--- mendelu-zoozs2022_xkolari1-c826e123d071/projekt/Interface/InputHandler.cpp
+++ mendelu-zoozs2022_xkolari1-c826e123d071/projekt/Interface/InputHandler.cpp
@@ -14,7 +14,7 @@
 
 #include "InputHandler.h"
 
-
+//Metoda pro získávání vstupu od hráče a porovnávání s přednastavenými konstantami.
 int InputHandler::getCommandForInput(int ascii) {
     //GameEngine::debugMessage(std::to_string(ascii));
     switch (ascii) {
